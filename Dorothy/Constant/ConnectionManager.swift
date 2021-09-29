68868d0b9fd2d204c96c867397d3ee3d1ca17488
Adarsh Raj <wbgios2021@gmail.com>
2021-09-29 19:55:21 +0530

    Fixing issues

---
+++ Dorothy/Constant/ConnectionManager.swift
@@ -0,0 +1,8 @@
+//
+//  ConnectionManager.swift
+//  Dorothy
+//
+//  Created by Satyam Kumar on 29/09/21.
+//
+
+import Foundation
---
+++ Dorothy/Controller/No Internet/NoInternetViewController.swift
@@ -0,0 +1,29 @@
+//
+//  NoInternetViewController.swift
+//  Dorothy
+//
+//  Created by Satyam Kumar on 29/09/21.
+//
+
+import UIKit
+
+class NoInternetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
